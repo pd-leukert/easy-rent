2ddb86fce50d2c8b90d9cd1ab2f760457324a106
LeQuadratico <45048954+LeQuadratico@users.noreply.github.com>
2022-10-02 19:56:09 +0200

    Add MUI design system

---
+++ constants/theme.js
@@ -0,0 +1,15 @@
+import { createTheme } from '@mui/material/styles';
+
+// Create a theme instance.
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#00CC66',
+        },
+        secondary: {
+            main: '#000000',
+        },
+    },
+});
+
+export default theme;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
-import '../styles/globals.css'
+import { CacheProvider, ThemeProvider } from '@emotion/react';
+import { CssBaseline } from '@mui/material';
+import Head from 'next/head';
+import theme from "../constants/theme";
+import createEmotionCache from '../utils/createEmotionCache';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const clientSideEmotionCache = createEmotionCache();
+
+function MyApp(props) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme} >
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
 }
 
 export default MyApp
